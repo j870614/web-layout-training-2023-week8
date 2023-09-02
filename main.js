5bbb68c968e1027342b3af48fd754958c2d1fb57
cookiesss123 <hoce527@gmail.com>
2023-09-02 22:46:06 +0800

    完成首頁區塊4

--- main.js
+++ main.js
@@ -2,3 +2,20 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 // 載入google font
 import 'material-icons/iconfont/material-icons.css';
+// 載入 swiper
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 24,
+    // mousewheel: true,
+    loop: false,
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    },
+});
